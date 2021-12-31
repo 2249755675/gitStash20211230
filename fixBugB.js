9b262f6d2facd655c0dbf722b4f8e57986a8c46e
chenjx <chenjiaxun22@163.com>
2021-12-31 11:34:21 +0800

    新增修改

--- fixBugB.js
+++ fixBugB.js
@@ -1,2 +1,4 @@
-//fixBugB分支的，新建了一个fixBugB文件，一会要stash切换//fixBugD分支修改原有
-//fixBugD分支对fixBugB.js文件修改，
+//fixBugB分支的，新建了一个fixBugB文件，一会要stash切换//fixBugD分支修改原有//新增修改
+//新增修改新增修改
+//fixBugD分支对fixBugB.js文件修改，//新增修改
+//新增修改新增修改
